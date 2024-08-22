e321d2458aefe3dd736f099bf69e7916852aeecd
kumar-guddu <guddu.kumar@aitoxr.ai>
2024-08-22 19:38:52 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,13 +7,13 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 8080,
-    // proxy: {
-    //   '/api': {
-    //     target: 'http://localhost:3001',
-    //     changeOrigin: true,
-    //     rewrite: (path) => path.replace(/^\/api/, ''),
-    //   },
-    // },
+    proxy: {
+      '/api': {
+        target: 'https://insighthread.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
   },
 
 })
